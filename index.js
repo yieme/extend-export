ddb113961b2f76eda7cec46d507b62d3f585bae4
yieme <yieme@cormea.com>
2015-05-10 12:38:48 -0600

    initial

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
  *
  *  @copyright  Copyright (C) 2015 by yieme
  *  @module     extend-export
+ *
+ *  @example    extendExport().parse('module.export = { foo: 'bar' }', 'enhancement')
+ *
+ *  var _ExtendedExport = { foo: 'bar' }
+ *  require('extendExport')(_ExtendedExport, 'enhancements')
+ *
  */
- (function() {
+(function() {
   'use strict';
+
+  var _  = require('lodash')
+  var fs = require('fs')
+
   function ExtendExportError(message) { // ref: https://stackoverflow.com/questions/1382107/whats-a-good-way-to-extend-error-in-javascript
     /*jshint validthis: true */
     this.constructor.prototype.__proto__ = Error.prototype
@@ -14,27 +24,64 @@
   }
 
   /** Extend export
-   *  @class
-   *  @param      {object} options - The options
-   *  @return     {object}
-   */
-  function extendExportClass(options) {
-    /*jshint validthis: true */
-    var self = this
-    options = options || {}
-    self.value = 'Hello, world'
-    return self
-  }
-
-
-
-  /** Extend export
    *  @constructor
    *  @param      {object} options - The options
    *  @return     {object}
    */
-  function extendExport(options) {
-    return new extendExportClass(options)
+  function extendExport(Export, extension) {
+    function parse(text, extension) {
+      if (typeof text === 'string') {
+        var spacing = ['', ' ', '  ', '   ', "\t"]
+        for (var i=0, len=spacing.length; i< len; i++) {
+          var search = 'module.export' + spacing[i] + '='
+          var part = text.split(search)
+          if (part[1]) {
+            text = part.join('var _ExtendExport =')
+            break
+          }
+        }
+        if (typeof extension === 'string') {
+          text += "\nrequire('extend-export')(_ExtendExport, '" + extension + "');"
+        }
+      }
+      return text
+    }
+
+    function parseFile(filepath, callback) {
+      fs.readFile(filePath, 'utf8', function(err, data) {
+        if (err) {
+          callback(err)
+        } else {
+          callback(null, parse(data))
+        }
+      })
+    }
+
+    function parseFileSync(filepath) {
+      try {
+        var text = fs.readFileSync(filePath, 'utf8')
+        return parse(text)
+      } catch (err) {
+        throw new ExtendExportError(err.message)
+      }
+    }
+
+    if (!Export) {
+      return {
+        parse:         parse,
+        parseFile:     parseFile,
+        parseFileSync: parseFileSync
+      }
+    }
+
+    try {
+      if (typeof extension === 'string') {
+        extension = require(extension)
+      }
+      return _.merge(Export, extension)
+    } catch (err) {
+      throw new ExtendExportError(err.message)
+    }
   }
 
 
--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
 var should = require('chai').should(),
-    extendExport = require('..')
-;
+    extendExport = require('..');
+var result = "var _ExtendExport = { foo: 'bar' }\nrequire('extend-export')(_ExtendExport, 'enhancement');";
 
 describe('extend-export', function() {
-  it('should say hello', function(done) {
-    var test = extendExport()
-    test.value.should.equal('Hello, world');
+  it('should parse', function(done) {
+    var test = extendExport().parse("module.export = { foo: 'bar' }", 'enhancement')
+    test.should.equal(result);
     done();
   });
 });
